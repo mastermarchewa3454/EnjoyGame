948061f140dc49e4a0e699a02eae4d496c93fcaf
mastermarchewa3454 <mastermarchewa3454@users.noreply.github.com>
2020-02-27 18:07:44 +0800

    Setting up starter pack

---
+++ Assets/Scripts/NextLevelDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+public class NextLevelDoor : MonoBehaviour
+{
+    SceneChanger sceneChanger;
+    public void Start()
+    {
+        sceneChanger = FindObjectOfType<SceneChanger>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {     
+        sceneChanger.ChangeToNextScene();
+    }
+}
---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int theCurrentScene;
+    public void ChangeToNextScene()
+    {
+        theCurrentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(theCurrentScene + 1);
+    }
+    public void ChangeToStartScene()
+    {
+        SceneManager.LoadScene(0); 
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+}
