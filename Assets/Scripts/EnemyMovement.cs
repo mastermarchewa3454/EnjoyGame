31274af39286c6a79b96e9af72584c10fcd9b7b9
Wonn Jen <lee.wonnjen@gmail.com>
2020-02-28 21:13:24 +0800

    Edit enemy AI

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float speed = 1;
-    public float directionChangeInterval = 1;
+    public float movementInterval = 1;
+    public float stopInterval = 2;
 
     public Rigidbody2D rb;
+    Animator anim;
 
     Vector2 movement;
+    bool moving;
 
-    void Awake()
+    void Start()
     {
+        anim = GameObject.Find("Sprite").GetComponent<Animator>();
         StartCoroutine(NewHeading());
     }
 
@@ -23,16 +27,34 @@ public class EnemyMovement : MonoBehaviour
 
     IEnumerator NewHeading()
     {
+        moving = false;
         while (true)
         {
-            NewHeadingRoutine();
-            yield return new WaitForSeconds(directionChangeInterval);
+            moving = !moving;
+            anim.SetBool("isMoving", moving);
+
+            if (moving)
+            {
+                NewHeadingRoutine();
+                yield return new WaitForSeconds(movementInterval);
+            }
+            else
+            {
+                StopHeading();
+                yield return new WaitForSeconds(stopInterval);
+            }
         }
+    }
+
+    void StopHeading()
+    {
+        movement.x = 0;
+        movement.y = 0;
     }
 
     void NewHeadingRoutine()
     {
-        movement.x = Random.Range(-1, 2);
-        movement.y = Random.Range(-1, 2);
+        movement.x = Random.Range(-1f, 1f);
+        movement.y = Random.Range(-1f, 1f);
     }
 }
