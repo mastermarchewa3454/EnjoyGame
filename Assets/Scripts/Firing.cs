f2a80b75f256c2b8edfa76b4b75d0a5ed40c28ce
Wonn Jen <lee.wonnjen@gmail.com>
2020-02-28 23:07:33 +0800

    Added (glitchy) shooting capabilities

---
+++ Assets/Scripts/Firing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour
+{
+    public float bulletForce = 20f;
+
+    public Transform firePoint;
+    public GameObject projectilePrefab;
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+    }
+
+    void Shoot()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D projectileRb = projectile.GetComponent<Rigidbody2D>();
+        projectileRb.AddForce(firePoint.right * -rb.transform.localScale.x * bulletForce, ForceMode2D.Impulse);
+    }
+}
