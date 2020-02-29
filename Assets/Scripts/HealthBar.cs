2bd46c150edf37cceed8db5293ef3e958cd0ae88
Wonn Jen <lee.wonnjen@gmail.com>
2020-02-29 15:12:56 +0800

    Add health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    Transform bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    void SetSize(float sizeNormalized)
+    {
+        bar.localScale = new Vector2(sizeNormalized, 1f);
+    }
+}
