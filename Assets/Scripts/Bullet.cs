8c7006f81f0106ff05aade6e8d00ce1ad5cb197c
Wonn Jen <lee.wonnjen@gmail.com>
2020-03-02 21:46:03 +0800

    Remove multiplayer

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class Bullet : NetworkBehaviour
+public class Bullet : MonoBehaviour
 {
     public int damage = 5;
 
