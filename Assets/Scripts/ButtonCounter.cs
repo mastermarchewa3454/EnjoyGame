f05d4886bba76ab81a73252ab3ac363b66ba434b
kuroducky <kuroducky@gmail.com>
2020-03-04 00:58:00 +0800

    Q&A canvas
    Finished up QNA for canvas. Included counter option and text fields

---
+++ Assets/Scripts/ButtonCounter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+ 
+public class ButtonCounter : MonoBehaviour 
+{
+    public Text counterText; 
+    public int counterValue=1;
+    public void counter()
+    {
+        if(counterValue == 20)
+        {
+            return;
+        }
+        else 
+        {
+            counterValue++;  
+            counterText.text = counterValue.ToString() + "/20";
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private InputField questionInput;
+    [SerializeField]
+    private InputField answerInput;
+    [SerializeField]
+    private Button submitButton;
+
+    public void getQuestion(string question)
+    {
+        Debug.Log("Your question is " + question);
+    }
+
+    public void getAnswer(string answer)
+    {
+        Debug.Log("Your answer is " + answer);
+    }
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("Button Selected");
+        questionInput.text = "";
+        answerInput.text = "";
+    }
+}
