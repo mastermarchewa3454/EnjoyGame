5ff9fc761866aa32c10cad047fe525ffd65cef28
hanliong-looi <hanliong99@gmail.com>
2020-03-03 12:10:30 +0800

    redo leaderboard

--- Assets/Scripts/Leaderboard/LeaderboardSaveData.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections.Generic;
-
-[System.Serializable]
-public class LeaderboardSaveData{
-    public List<LeaderboardEntryData> highscores = new List<LeaderboardEntryData>();
-}
