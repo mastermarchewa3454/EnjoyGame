2af5d2a5bf83ba4a8a5a2da506d723b4acf900ca
kuroducky <kuroducky@gmail.com>
2020-03-05 02:52:23 +0800

    a

---
+++ Assets/Scripts/LoginRegister/RegisterDropdown.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterDropdown : MonoBehaviour
+{
+    private bool isClassSelected = false;
+    private string classSelected; 
+
+    public void HandleInputData(int val)
+    {
+        switch(val){
+            case 1:
+            Debug.Log("Class A");
+            isClassSelected = true;
+            classSelected = "Class A";
+            break;
+            case 2:
+            Debug.Log("Class B");
+            isClassSelected = true;
+            classSelected = "Class B";
+            break;
+            case 3:
+            Debug.Log("Class C");
+            isClassSelected = true;
+            classSelected = "Class C";
+            break;
+            default:
+            Debug.Log("Select Your Class");
+            isClassSelected = false;
+            classSelected = null;
+            break;
+            
+        }
+    }
+
+}
