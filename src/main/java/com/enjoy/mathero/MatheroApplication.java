49feedaada1f31289119d0d9afa9e3cb722e2843
Kamil Rogoda <rogoda.kamil@gmail.com>
2020-03-12 02:41:48 +0800

    Init commit for webservice

---
+++ src/main/java/com/enjoy/mathero/MatheroApplication.java
@@ -0,0 +1,13 @@
+package com.enjoy.mathero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enjoy/mathero/MatheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enjoy.mathero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
