eaaea6c0132f37390df6b4db87f99563a1c7f224
Kamil Rogoda <rogoda.kamil@gmail.com>
2020-03-12 14:21:12 +0800

    Connect User to DB

---
+++ src/main/java/com/enjoy/mathero/UserRepository.java
@@ -0,0 +1,10 @@
+package com.enjoy.mathero;
+
+import com.enjoy.mathero.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findUserByEmail(String email);
+}
--- src/main/java/com/enjoy/mathero/service/impl/UserServiceImpl.java
+++ src/main/java/com/enjoy/mathero/service/impl/UserServiceImpl.java
@@ -1,13 +1,33 @@
 package com.enjoy.mathero.service.impl;
 
+import com.enjoy.mathero.UserRepository;
+import com.enjoy.mathero.io.entity.UserEntity;
 import com.enjoy.mathero.service.UserService;
 import com.enjoy.mathero.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("dupa");
+        userEntity.setUserId("test id");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
+
+        return returnValue;
     }
 }
