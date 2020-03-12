e824792be0e2792bb9a72447aa3464205ff054a5
Kamil Rogoda <rogoda.kamil@gmail.com>
2020-03-12 14:39:46 +0800

    Generate random public User id

--- src/main/java/com/enjoy/mathero/service/impl/UserServiceImpl.java
+++ src/main/java/com/enjoy/mathero/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.enjoy.mathero.service.impl;
 import com.enjoy.mathero.UserRepository;
 import com.enjoy.mathero.io.entity.UserEntity;
 import com.enjoy.mathero.service.UserService;
+import com.enjoy.mathero.shared.Utils;
 import com.enjoy.mathero.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +15,20 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
+
+        if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
 
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
+        String publicUserId = utils.generateUserID(30);
         userEntity.setEncryptedPassword("dupa");
-        userEntity.setUserId("test id");
+        userEntity.setUserId(publicUserId);
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
