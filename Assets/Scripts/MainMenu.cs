2379fa889fce0c61553198411d0d4d1ee2f292fe
kuroducky <kuroducky@gmail.com>
2020-03-15 22:34:44 +0800

    Removed scripts that were previously not used and updated the scripts with docs
    Left with a little bit more documentation for my part :)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Contains the methods that the MainMenu holds and utilizes 
+/// </summary>
 public class MainMenu : MonoBehaviour
 {
     public void Start()
@@ -13,52 +16,78 @@ public class MainMenu : MonoBehaviour
         PlayerPrefs.DeleteKey("treasure");
     }
 
-
+    /// <summary>
+    /// Method call to Start the game
+    /// </summary>
     public void PlayGame()
     {
         SceneManager.LoadScene("StageSelection");
     }
 
+    /// <summary>
+    /// Method call to view the leaderboard
+    /// </summary>
     public void Leaderboard()
     {
         SceneManager.LoadScene("Leaderboard");
     }
 
+    /// <summary>
+    /// Method call to End the game.
+    /// </summary>
     public void QuitGame()
     {
         Debug.Log("Sucessfully Exitted");
         Application.Quit();
     }
-
+    /// <summary>
+    /// Method call to return to the main menu.
+    /// </summary>
     public void ReturnMainMenu()
     {
         SceneManager.LoadScene("StartScreen");
     }
-
+    /// <summary>
+    /// Method call to enter the social menu of the game.
+    /// </summary>
     public void EnterSocialMenu()
     {
         SceneManager.LoadScene("Social");
     }
-
+    /// <summary>
+    /// Method call to enter custom creation lobby to create a custom game.
+    /// </summary>
     public void EnterCustomLobby()
     {
         SceneManager.LoadScene("QnA");
     }
-
+    /// <summary>
+    /// Method call to choose the class and students to view the report for the teacher.
+    /// </summary>
     public void EnterTeacherReport()
     {
         SceneManager.LoadScene("TeacherReport");
     }
-
+    /// <summary>
+    /// Method call to enter the main menu screen for the teacher.
+    /// </summary>
     public void EnterTeacherScreen()
     {
         SceneManager.LoadScene("TeacherScreen");
     }
-
+    /// <summary>
+    /// Method call to view the summary reports that will include 
+    /// 1)Stages cleared
+    /// 2)Questions answer 
+    /// 3)Overall progress report
+    /// </summary>
     public void EnterReportSummary()
     {
         SceneManager.LoadScene("SummaryReportDetails");
     }
+    /// <summary>
+    /// Method call to redirect the user back to the login page.
+    /// </summary>
     public void EnterLogIn()
     {
         SceneManager.LoadScene("LoginScreen");
--- Assets/Scripts/SocialMedia/Connection.cs
+++ Assets/Scripts/SocialMedia/Connection.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Connection class that will contain the methods/functions to 
+/// call the specific function upon clicking on the button
+/// </summary>
+
 public class Connection : MonoBehaviour
 {
+    /// <summary>
+    /// Opens up a link to Facebook after clicking on it
+    /// </summary>
     public void OpenFacebook()
     {
         Application.OpenURL("https://www.facebook.com/");
     }
+    /// <summary>
+    /// Opens up a link to Instagram after clicking on it
+    /// </summary>
     public void OpenInstagram()
     {
         Application.OpenURL("https://www.instagram.com/");
     }
+    /// <summary>
+    /// Opens up a link to WhatsApp after clicking on it
+    /// </summary>
     public void OpenWhatsApp()
     {
         Application.OpenURL("https://web.whatsapp.com/"); 
