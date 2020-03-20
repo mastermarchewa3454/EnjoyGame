6ed75fd40a857c796e2461c3f2d0c841cb952872
lyeeteng1999 <lyeeteng1999@gmail.com>
2020-03-21 00:28:11 +0800

    added scripts

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public int QuestionID;
+        public int DifficultyLevel;
+        public string Question;
+        public string Answer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
