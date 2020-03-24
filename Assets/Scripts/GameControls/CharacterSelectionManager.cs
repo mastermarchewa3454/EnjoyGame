0a538f1c5fc4872722378a7d874053abc44a1138
Wonn Jen <lee.wonnjen@gmail.com>
2020-03-25 02:24:55 +0800

    Added the other characters into the game

--- Assets/Scripts/GameControls/CharacterSelectionManager.cs
+++ Assets/Scripts/GameControls/CharacterSelectionManager.cs
@@ -5,23 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class CharacterSelectionManager : MonoBehaviour
 {
-    public int characterChoice;
     public void ChooseArcher()
     {
+        PlayerPrefs.SetInt("character", 1);
         SceneManager.LoadScene("StageSelection");
-        characterChoice = 1;
     }
 
     public void ChooseMole()
     {
+        PlayerPrefs.SetInt("character", 2);
         SceneManager.LoadScene("StageSelection");
-        characterChoice = 2;
     }
 
     public void ChooseTreant()
     {
+        PlayerPrefs.SetInt("character", 3);
         SceneManager.LoadScene("StageSelection");
-        characterChoice = 3;
     }
 
 }
---
+++ Assets/Scripts/GameControls/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject archer;
+
+    [SerializeField]
+    GameObject mole;
+
+    [SerializeField]
+    GameObject treant;
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (PlayerPrefs.GetInt("character", 1))
+        {
+            case 1: player = archer; break;
+            case 2: player = mole; break;
+            case 3: player = treant; break;
+            default: player = archer; break;
+        }
+
+        player = Instantiate(player, transform.position, Quaternion.identity);
+        player.name = "Player";
+    }
+
+}
