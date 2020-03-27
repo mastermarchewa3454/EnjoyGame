fa164b07f804499a5d0412ed02b37f227edf64b2
lyeeteng1999 <lyeeteng1999@gmail.com>
2020-03-27 23:14:42 +0800

    added question difficulty

--- Assets/Scripts/GameControls/RandomWord.cs
+++ Assets/Scripts/GameControls/RandomWord.cs
@@ -25,9 +25,26 @@ public class RandomWord : MonoBehaviour
     /// <returns>Difficulty</returns>
     private string PickRandomDifficulty()
     {
+        //get number of correct answered questions
+        //int easy = 
+        //int medium = 
+        //int hard =
+        //int total = easy + medium+  hard;
+        //if (total<3){
         string[] difficulty = new string[] { "Easy", "Medium", "Hard" };
         string randomdifficulty = difficulty[Random.Range(0, difficulty.Length)];
         return randomdifficulty;
+        //}
+        //else{
+        //  easypercent = easy/total*100;
+        //  mediumpercent = medium/total*100;
+        //  hardpercent = hard/total*100;
+        //  if(easypercent>30 || hardpercent>40){
+        //      string[] difficulty = new string[] { "Medium", "Hard" };
+        //      string randomdifficulty = difficulty[Random.Range(0, difficulty.Length)];
+        //      return randomdifficulty;
+        //  }
+        //}
     }
 
     /// <summary>
