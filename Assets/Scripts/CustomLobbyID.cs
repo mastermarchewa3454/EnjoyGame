6393497d40dd34134c1aeda0e4b0288b9be940b3
kuroducky <kuroducky@gmail.com>
2020-04-16 18:00:01 +0800

    slowly try...
    slowly trying....

---
+++ Assets/Scripts/CustomLobbyID.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CustomLobbyID : MonoBehaviour
+{
+    public TMP_Text customlobbyID;
+
+    public void Start()
+    {
+        var ID = PlayerPrefs.GetString("SavedCLI");
+        Debug.Log("Your custom lobby id is " + ID);
+        customlobbyID.text = "Your Custom Lobby has been successfully created with ID : " + ID;
+    }
+}
