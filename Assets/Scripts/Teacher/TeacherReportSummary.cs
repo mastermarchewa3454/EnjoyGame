47b3fc652bb0ecbdb5e3fa94d219448860df6dc5
Wonn Jen <lee.wonnjen@gmail.com>
2020-04-18 17:10:01 +0800

    Fixing merge conflict

---
+++ Assets/Scripts/Teacher/TeacherReportSummary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TeacherReportSummary : MonoBehaviour
+{
+    public Dropdown dropdown;
+    public Text selectedName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopulateList(); 
+    }
+
+    public void PopulateList()
+    {
+        List<string> names = new List<string>()
+        {
+
+        };
+        dropdown.AddOptions(names);
+    }
+}
