a36093930d102007cc98a11986cbc3f3c46772b3
kuroducky <kuroducky@gmail.com>
2020-04-18 17:10:09 +0800

    added error message.
    added error message.

--- Assets/Scripts/Database/DBTeacherManager.cs
+++ Assets/Scripts/Database/DBTeacherManager.cs
@@ -1,13 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DBTeacherManager : MonoBehaviour
-{
-    public IEnumerator GetTeacher(System.Action callback)
-    {
-        string teacherString = "";
-        yield return StartCoroutine(GetData("/teachers/" + userId, callback: data => teacherString = data));
-        Teacher teacher = JsonUtility.FromJson<Teacher>(teacherString);
-    }
-}
+﻿
--- Assets/Scripts/Teacher/TeacherReportSummary.cs
+++ Assets/Scripts/Teacher/TeacherReportSummary.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 
 public class TeacherReportSummary : MonoBehaviour
 {
-    public Dropdown dropdown;
-    public Text selectedName;
+    public TMP_Dropdown dropdown;
+    public TMP_Text teacherName;
+    public TMP_Text className;
 
     // Start is called before the first frame update
     void Start()
     {
-        PopulateList; 
+        PopulateList();
+
+        teacherName.text = "hello";
+        className.text = "3a";
     }
 
     public void PopulateList()
     {
         List<string> names = new List<string>()
         {
-
+            "View All", "Betty", "Funny", "Hello" 
         };
         dropdown.AddOptions(names);
     }
+
+
 }
