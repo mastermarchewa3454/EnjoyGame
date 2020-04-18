90e7f1193fdfccf5dd1b6c9a21e1f6b797d85560
Wonn Jen <lee.wonnjen@gmail.com>
2020-04-18 17:57:09 +0800

    Link up with db

--- Assets/Scripts/Teacher/TeacherReportSummary.cs
+++ Assets/Scripts/Teacher/TeacherReportSummary.cs
@@ -10,21 +10,39 @@ public class TeacherReportSummary : MonoBehaviour
     public TMP_Text teacherName;
     public TMP_Text className;
 
+    Teacher teacher;
+    Class classes;
+    DBUserManager db;
+
     // Start is called before the first frame update
     void Start()
     {
-        PopulateList();
+        db = GetComponent<DBUserManager>();
+        
+        StartCoroutine(PopulateList());
 
         teacherName.text = "hello";
         className.text = "3a";
     }
 
-    public void PopulateList()
+    /*public void PopulateList()
     {
         List<string> names = new List<string>()
         {
             "View All", "Betty", "Funny", "Hello" 
         };
+        dropdown.AddOptions(names);
+    }*/
+
+    IEnumerator PopulateList()
+    {
+        yield return StartCoroutine(db.GetTeacher(callback:data => teacher = data));
+        yield return StartCoroutine(db.GetClasses(teacher.teaches, callback: data => classes = data));
+        List<string> names = new List<string>();
+        foreach (Student s in classes.students)
+        {
+            names.Add(s.firstName);
+        }
         dropdown.AddOptions(names);
     }
 }
